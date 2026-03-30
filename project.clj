cf5306b310101f02b251d718873ec4d000400210
JordanSkousen <jotheman21@gmail.com>
2026-03-29 18:38:33 -0600

    Cleanup pt 2

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject com.jothedev/re-re-frame "0.1.0"
-  :description "The last missing piece of re-frame"
-  :url "https://github.com/JordanSkousen/re-re-frame"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.4"]
-                 [re-frame/re-frame "1.4.4"]]
-  :repl-options {:init-ns re-re-frame.core})
